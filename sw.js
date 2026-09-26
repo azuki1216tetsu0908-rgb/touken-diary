51e753e7ab8b966dc86cc2535964ce6492278172
azuki1216tetsu0908-rgb <azuki1216tetsu0908@gmail.com>
2026-09-26 16:27:04 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="touken-diary-v17";
+const CACHE="touken-diary-v18";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icon.svg"];
 
 self.addEventListener("install", event => {
